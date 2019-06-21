223953be4ac083c43ae54afdde23bda35a3854c3
Aqil Atakishiyev <agilea@code.edu.az>
2019-06-21 18:02:43 +0400

    First commit Final Project Practice

---
+++ js/main.js
@@ -0,0 +1,83 @@
+// const div = document.getElementById("main");
+
+// $("#main").click(function(){
+//     alert("salam");
+// });
+
+
+// if(div != null){
+//     div.addEventListener('click', function(){
+//         alert("salam");
+//     });
+// }
+
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel();
+// });
+// const autoplay = true;
+
+
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     dots: true,
+//     autoplay:true,
+//     autoplayTimeout:3000,
+//     autoplayHoverPause:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:10
+//         }
+//     }
+// })
+// $('.loop').owlCarousel({
+//     center: true,
+//     items:2,
+//     loop:true,
+//     margin:10,
+//     center: true,
+//     responsive:{
+//         600:{
+//             items:4
+//         }
+//     }
+// });
+// $('.nonloop').owlCarousel({
+//     center: true,
+//     items:2,
+//     loop:false,
+//     margin:10,
+//     responsive:{
+//         600:{
+//             items:4
+//         }
+//     }
+// });
+
+// //function declaration
+// function MyFunction (){
+//     alert("salam");
+// }
+// MyFunction();
+
+// //function expression
+// YourFunction();
+// const YourFunction = function(){
+//     alert("sagol");
+// }
+
+
+let OurFunction;
+
+OurFunction = (a, b) => alert(a+b);
+OurFunction(5, 5);
+
+OurFunction = (a, b) => alert(a - b);
+OurFunction(100, 50);
